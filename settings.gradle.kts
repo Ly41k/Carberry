76c13b2465c7839d2bf5b9fd4d1377e8538e3497
YuriyG <mym21072011@gmail.com>
2024-05-27 18:39:06 +0300

    Start project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "Carberry"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
