a8927e1a5be323b52c50fc8a3856d0e8382f5be6
YuriyG <mym21072011@gmail.com>
2024-06-20 19:53:05 +0300

    Update splash logo

--- composeApp/src/commonMain/kotlin/pro/carberry/multiplatform/features/splash/compose/SplashView.kt
+++ composeApp/src/commonMain/kotlin/pro/carberry/multiplatform/features/splash/compose/SplashView.kt
@@ -1,18 +1,20 @@
 package pro.carberry.multiplatform.features.splash.compose
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import carberry.composeapp.generated.resources.Canavar
 import carberry.composeapp.generated.resources.Res
-import carberry.composeapp.generated.resources.ic_logo_vertical
-import org.jetbrains.compose.resources.painterResource
+import carberry.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.Font
+import org.jetbrains.compose.resources.stringResource
 import pro.carberry.multiplatform.theme.AppTheme
 
 @Composable
@@ -22,11 +24,13 @@ fun SplashView() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            modifier = Modifier.size(150.dp),
-            painter = painterResource(Res.drawable.ic_logo_vertical),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(AppTheme.colors.primaryAction)
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(Res.string.app_name),
+            textAlign = TextAlign.Center,
+            color = AppTheme.colors.primaryText,
+            style = AppTheme.typography.largeHeading,
+            fontFamily = FontFamily(Font(Res.font.Canavar))
         )
     }
 }
