9453dc91cf47c26cf1dcaf52e2efbde16d91387e
YuriyG <mym21072011@gmail.com>
2024-06-28 19:10:42 +0300

    Add loader and update app icon for android side

--- composeApp/src/commonMain/kotlin/pro/carberry/multiplatform/features/splash/compose/SplashView.kt
+++ composeApp/src/commonMain/kotlin/pro/carberry/multiplatform/features/splash/compose/SplashView.kt
@@ -3,18 +3,12 @@ package pro.carberry.multiplatform.features.splash.compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
-import carberry.composeapp.generated.resources.Canavar
-import carberry.composeapp.generated.resources.Res
-import carberry.composeapp.generated.resources.app_name
-import org.jetbrains.compose.resources.Font
-import org.jetbrains.compose.resources.stringResource
+import androidx.compose.ui.unit.dp
+import pro.carberry.multiplatform.core.compose.loader.CircleLoader
 import pro.carberry.multiplatform.theme.AppTheme
 
 @Composable
@@ -24,13 +18,10 @@ fun SplashView() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = stringResource(Res.string.app_name),
-            textAlign = TextAlign.Center,
-            color = AppTheme.colors.primaryText,
-            style = AppTheme.typography.largeHeading,
-            fontFamily = FontFamily(Font(Res.font.Canavar))
+        CircleLoader(
+            color = AppTheme.colors.primaryAction,
+            modifier = Modifier.size(96.dp),
+            isVisible = true
         )
     }
 }
