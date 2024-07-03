df704cd24dd92dd3f574454bef817a9eba4deaf6
YuriyG <mym21072011@gmail.com>
2024-07-03 23:24:52 +0300

    Update version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -110,8 +110,8 @@ android {
         targetSdk = 34
 
         applicationId = "pro.carberry.multiplatform.androidApp"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
